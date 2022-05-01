d2f3d27ce94baaa7723ffabff62193faa27cf885
Bogdan191 <bogdanpintilie00@yahoo.com>
2022-05-01 18:38:07 +0300

    Update My car details activity

--- app/src/main/java/com/example/mygarage/CarRUDActivity.java
+++ app/src/main/java/com/example/mygarage/CarRUDActivity.java
@@ -2,23 +2,28 @@ package com.example.mygarage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mygarage.models.CarModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CarRUDActivity extends AppCompatActivity {
 
-    TextView textViewCarDetails;
+    TextView textViewCarName;
+    TextView tvCarManufacturedData;
+    TextView tvCarOdometer, tvCarEngine, tvCarHP, tvCarEmission, tvCarRimSize, tvCarGearbox, tvCarMarketValue;
+
     ImageView ivCarDetails;
     Button buttonDeleteCar;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,12 +36,31 @@ public class CarRUDActivity extends AppCompatActivity {
         DBHelper dbHelper = new DBHelper(CarRUDActivity.this);
         CarModel car = dbHelper.getCarById(carId);
 
-        textViewCarDetails = findViewById(R.id.textViewCarDetails);
+        textViewCarName = findViewById(R.id.tvCarName);
         ivCarDetails = findViewById(R.id.imageViewCarDetails);
+        tvCarManufacturedData = findViewById(R.id.tvCarManufacturedData);
+        tvCarOdometer = findViewById(R.id.tvCarOdometer);
+        tvCarEngine = findViewById(R.id.tvCarEngine);
+        tvCarHP = findViewById(R.id.tvCarHP);
+        tvCarEmission = findViewById(R.id.tvCarEmission);
+        tvCarRimSize = findViewById(R.id.tvCarRimSize);
+        tvCarGearbox = findViewById(R.id.tvCarGearbox);
+        tvCarMarketValue = findViewById(R.id.tvCarMarketValue);
+
         buttonDeleteCar = findViewById(R.id.buttonDeleteCarFromDB);
 
-        textViewCarDetails.setText(car.toString());
+        //afiseaza datele masinii in campurile aferente
+        textViewCarName.setText(car.getMake() + " " + car.getModel());
         ivCarDetails.setImageBitmap(Utils.getImage(car.getCarImage()));
+        tvCarManufacturedData.setText(car.getManufactured_date());
+        tvCarOdometer.setText(car.getOdometer()+" km");
+        tvCarEngine.setText(car.getEngine_capacity());
+        tvCarHP.setText(car.getHorse_power() + " CP");
+        tvCarEmission.setText(car.getEmission_standard());
+        tvCarRimSize.setText(car.getRim_size());
+        tvCarGearbox.setText(car.isManual_gearbox()  == true ? "Cutie de viteze manuala" : "Cutie de viteze automata");
+        tvCarMarketValue.setText("Valoarea curenta a masinii: " + car.getCurrent_market_value() + "€");
+
         buttonDeleteCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,8 +68,30 @@ public class CarRUDActivity extends AppCompatActivity {
             }
         });
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu_my_car_details);
+        bottomNavigationView.setSelectedItemId(R.id.my_car_details);
 
-        this.setTheme(R.style.Theme_Design_Light_NoActionBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+                    case R.id.my_car_details:
+                        Toast.makeText(getApplicationContext(), "Sunteti deja pe aceasta sectiune!", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.my_car_docs:
+                        Toast.makeText(getApplicationContext(), "Aceasta optiune nu este disponibila momentan!", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.my_car_service_history:
+                        Toast.makeText(getApplicationContext(), "Aceasta optiune nu este disponibila momentan", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
+                }
+
+            }
+
+        });
 
     }
 
